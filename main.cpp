62994ec3f16a2653db40f56a5e9980a51a1410ae
berndlorenz <berndlorenz85@users.noreply.github.com>
2023-06-22 15:15:22 +0000

    Continuous reading of Cell voltages with ReadAll commands in 1s interval

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "mbed.h"
 #include "Scheduler/Scheduler.h"
+#include "MAX32620FTHR.h"
+
+ // Initialize I/O voltages on MAX32620FTHR board
+ MAX32620FTHR fthr(MAX32620FTHR::VIO_3V3);
 
 Serial pc(USBTX,USBRX,115200); // 3rd parameter is baud rate
 
+
 int main()
 {
-    printf("MBED VERSION %d.%d.%d\r\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    pc.printf("MBED VERSION %d.%d.%d\r\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     if(Scheduler_Init())
     {
-        printf("Initialization failed, Device restarts/n");
+        pc.printf("Initialization failed, Device restarts/n");
         wait_ms(1000);
         NVIC_SystemReset();
         //printf Failure message
